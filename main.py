4c646e18bd119ae07dcd500bcf3a0d4acdc9f6b4
unknown <raphdumape@yahoo.com>
2014-10-09 01:49:01 +0800

    Applying Phase 2 edits and redesigning the whole page

--- main.py
+++ main.py
@@ -8,17 +8,44 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 import logging
+from google.appengine.api import urlfetch
+import json
 
+FACEBOOK_APP_ID = "788020947916359"
+FACEBOOK_APP_SECRET = "696c6ad64c056ec2b03bde035d9760bd"
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class User(ndb.Model):
+	user_id = ndb.StringProperty(required=True)
+	access_token = ndb.StringProperty(required=True)
+
+class Posts(ndb.Model):
+    user_id = ndb.StringProperty(required=True)
+    access_token = ndb.StringProperty(required=True)
+    message = ndb.StringProperty(required=True)
+    date_to_post = ndb.DateProperty()
+    date_created = ndb.DateProperty()
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('templates/main.html')
         self.response.write(template.render())
+    def post(self):
+        data = {
+                    "method": "post",
+                    "message": self.request.get("message"),
+                    "access_token": self.request.get("access_token")
+                };
+    	if(content.get("id")):
+           self.response.write('<script>alert("Message posted to facebook.");window.location.assign("/")</script>')
+        elif content["error"]["error_user_title"]:
+            self.response.write('<script>alert("'+content["error"]["error_user_title"]+'");window.location.assign("/")</script>')
+        else:
+            self.response.write('<script>alert("An error occured.");window.location.assign("/")</script>')
 
 
 application = webapp2.WSGIApplication([
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,149 @@
-window.fbAsyncInit = function() { FB.init({ appId      : '788020947916359', xfbml      : true, version    : 'v2.1' }); start(); }; (function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/sdk.js"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));   function showSuccessToast() { $().toastmessage('showSuccessToast', "Success! Check your FB Post"); }; function showStickySuccessToast() { $().toastmessage('showToast', { text     : 'Success Dialog which is sticky', sticky   : true, position : 'top-right', type     : 'success', closeText: '', close    : function () { console.log("toast is closed ..."); } }); };  function showNoticeToast() { $().toastmessage('showNoticeToast', "Notice  Please Login"); };  function showStickyNoticeToast() { $().toastmessage('showToast', { text     : 'Notice Dialog which is sticky', sticky   : true, position : 'top-right', type     : 'notice', closeText: '', close    : function () {console.log("toast is closed ...");} }); };  function showWarningToast() { $().toastmessage('showWarningToast', "Oh Snap!.. There's something wrong :X "); }; function showStickyWarningToast() { $().toastmessage('showToast', { text     : 'Warning Dialog which is sticky', sticky   : true, position : 'top-right', type     : 'warning', closeText: '', close    : function () { console.log("toast is closed ..."); } }); };  function showErrorToast() { $().toastmessage('showErrorToast', "Error Dialog which is fading away ..."); }; function showStickyErrorToast() { $().toastmessage('showToast', { text     : 'Error Dialog which is sticky', sticky   : true, position : 'top-right', type     : 'error', closeText: '', close    : function () { console.log("toast is closed ..."); } }); };  function start() { var user; var baseUrl = "https://graph.facebook.com/v2.1/"; getLoginStatus(); $("#fb-login").click(function(){ getLoginStatus(login); }); $("#fb-logout").click(logout); $("#postmessage").submit(function(){ if(user){ var msg = $("#message").val(); postToFB(msg); return false; }else{ showNoticeToast(); return false; } });  function getLoginStatus(callback){ FB.getLoginStatus(function(response){ if(response.status=="connected"){ getFBresponse(response); toggleLogin(); }else if(typeof callback === 'function' && callback()){ callback(response); } }); };  function postToFB(msg){ var url = baseUrl + user.userID + "/feed/"; var data = { method: "post", message: msg, access_token: user.accessToken }; $.get(url,data,function(response){ if(response.id){ showSuccessToast(); var msg = $("#post-form textarea").val(""); }else{ showWarningToast(); } }); }; function getFBresponse(response){ user=response.authResponse; };  function login(){ FB.login(function(response){ if(response.authResponse){ getFBresponse(response); toggleLogin(); } }, {scope: 'publish_actions',return_scopes:true}); };  function logout(){ FB.logout(function(){ toggleLogin(); user=null; }); };  function toggleLogin(){ $("#fb-login,#fb-logout").toggle(); }; };
+window.fbAsyncInit = function() {
+ FB.init({ appId      : '788020947916359',
+  xfbml      : true,
+  version    : 'v2.1' });
+   start();
+    };
+ (function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {
+   	return;
+   }
+   js = d.createElement(s);
+   js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+     }
+     (document, 'script', 'facebook-jssdk'));
+function showSuccessToast() {
+ $().toastmessage('showSuccessToast', "Success! Check your FB Post");
+  }; 
+  function showStickySuccessToast() {
+   $().toastmessage('showToast', { 
+   	text     : 'Success Dialog which is sticky',
+   	 sticky   : true, 
+   	 position : 'top-right', 
+   	 type     : 'success', 
+   	 closeText: '', 
+   	 close    : function () {
+   	  console.log("toast is closed ...");
+   	   }
+   	    });
+   	     };
+function showNoticeToast() {
+ $().toastmessage('showNoticeToast', "Notice  Please Login");
+  };
+function showStickyNoticeToast() {
+ $().toastmessage('showToast', {
+  text     : 'Notice Dialog which is sticky',
+  sticky   : true, 
+  position : 'top-right', 
+  type     : 'notice', 
+  closeText: '', 
+  close    : function () {
+  	console.log("toast is closed ...");
+  }
+   });
+    };
+function showWarningToast() {
+ $().toastmessage('showWarningToast', "Oh Snap!.. There's something wrong :X ");
+  };
+function showStickyWarningToast() {
+ $().toastmessage('showToast', { 
+ 	text     : 'Warning Dialog which is sticky', 
+ 	sticky   : true, 
+ 	position : 'top-right', 
+ 	type     : 'warning', 
+ 	closeText: '', 
+ 	close    : function () {
+ 	 console.log("toast is closed ...");
+ 	  }
+ 	   });
+ 	    };
+function showErrorToast() {
+ $().toastmessage('showErrorToast', "Error Dialog which is fading away ...");
+  };
+function showStickyErrorToast() {
+ $().toastmessage('showToast', {
+  text     : 'Error Dialog which is sticky', 
+  sticky   : true, 
+  position : 'top-right', 
+  type     : 'error', 
+  closeText: '', 
+  close    : function () {
+   console.log("toast is closed ...");
+    }
+     });
+      };
+function start() {
+ var user;
+ var baseUrl = "https://graph.facebook.com/v2.1/";
+  getLoginStatus();
+   $("#fb-login").click(function(){
+    getLoginStatus(login);
+     });
+      $("#fb-logout").click(logout);
+      $("#postmessage").submit(function(){
+       if(user){
+        var msg = $("#message").val();
+         postToFB(msg);
+          return false;
+           }
+           else{
+            showNoticeToast();
+             return false;
+              }
+               });
+function getLoginStatus(callback){
+ FB.getLoginStatus(function(response){
+  if(response.status=="connected"){
+   getFBresponse(response);
+    toggleLogin();
+     }
+     else if(typeof callback === 'function' && callback()){
+      callback(response);
+       }
+        });
+         };
+
+function postToFB(msg){
+ var url = baseUrl + user.userID + "/feed/";
+ var data = { 
+ 	method: "post", 
+ 	message: msg, 
+ 	access_token: user.accessToken 
+ }; 
+ $.get(url,data,function(response){
+  if(response.id){
+   showSuccessToast();
+    var msg = $("#post-form textarea").val("");
+     }
+     else{
+      showWarningToast();
+       }
+        });
+         };
+function getFBresponse(response){ 
+	user=response.authResponse;
+};
+function login(){ FB.login(function(response){
+ if(response.authResponse){
+  getFBresponse(response);
+   toggleLogin();
+    }
+     },
+      {
+      	scope: 'publish_actions',
+      	return_scopes:true});
+      	 };
+      	   function logout(){
+      	    FB.logout(function(){
+      	     toggleLogin();
+      	      user=null;
+      	       });
+      	        };
+      	          function toggleLogin(){
+      	           $("#fb-login,#fb-logout").toggle();
+      	            };
+      	             };
+
